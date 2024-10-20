b04f8ff418a5d8e7f6e1c8c73fb69431f7443998
Pari55051 <xpari5505@gmail.com>
2024-10-20 18:09:56 +0530

    update code as per requirements

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 
 let inputField = document.getElementById('text-input');
-let outputField = document.getElementById('output');
+let outputField = document.getElementById('result');
 // let text_input = input_box.value;
 
 // console.log(inputField)
 
 function isPalindrome () {
-    let textInput = inputField.value;
+    let textInput = inputField.value.toLowerCase();
+
+    if (!textInput) {
+        alert("Please input a value");
+        return false
+    }
 
     // removeSpecialCharacters(textInput)
-    let text = textInput.replace(/[^a-zA-Z0-9\s]/g, '');
+    let text = textInput.replace(/[^a-zA-Z0-9]/g, '');
 
-    // console.log(textInput)
-    // console.log(text)
+    console.log(textInput)
+    console.log(text)
 
     for (let i = 0; i < text.length; i++) {
         if (text[i] != text[text.length - 1 - i]) {
             console.log("not a palindrome")
-            outputField.innerHTML = `<p id="output"><span>${textInput}</span> is not a palindrome.</p>`
+            outputField.innerHTML = `<p id="result">${textInput} is not a palindrome</p>`
             return false
         }
     }
 
     console.log("Palindrome")
-    outputField.innerHTML = `<p id="output"><span>${textInput}</span> is a palindrome.</p>`
+    outputField.innerHTML = `<p id="result">${textInput} is a palindrome</p>`
     return true
 }
 
